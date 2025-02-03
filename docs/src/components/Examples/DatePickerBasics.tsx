c58c2883a00bdf5c4065d8cb96ff27c12574c3a9
Johan <johan.mickelin@gmail.com>
2025-02-03 16:13:21 +0100

    Deploy website - based on 6677a78d6cdbc8e0d84b29f39ddaa605234cf3fb

--- docs/src/components/Examples/DatePickerBasics.tsx
+++
@@ -1,7 +0,0 @@
-import DatePicker from "../../../../src/components/DatePicker"
-
-const DatePickerBasics = () => {
-  return <div className="bg-red-200 w-48">DatePickerBasics</div>
-}
-
-export default DatePickerBasics
